1b41b8cd31a76e678f04e48a4f03f848377a0951
Kevin Wooten <kevin@outfoxx.io>
2022-06-13 07:23:24 -0700

    Release 1.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 
 group = "io.outfoxx"
-version = "1.4.1"
+version = "1.4.2"
 description = "A Kotlin/Java API for generating .swift source files."
 
 val isSnapshot = "$version".endsWith("SNAPSHOT")
