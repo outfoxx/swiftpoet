deaec517def1c295cd08ed9209fb81b3ccb0bf89
oldergod <benoit@quenaudon.com>
2025-10-22 09:54:58 +0000

    Deploying to gh-pages from @ outfoxx/swiftpoet@3670d54184918cfc5cf3e3766a7ce73e736dd1b0 🚀

---
+++ module-search-index.js
@@ -0,0 +1,1 @@
+var moduleSearchIndex = [{"l":"swiftpoet","url":"index.html"}]
---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+var packageSearchIndex = [{"l":"io.outfoxx.swiftpoet","url":"io/outfoxx/swiftpoet/package-summary.html"}, {"l":"All packages","url":"index.html"}]
